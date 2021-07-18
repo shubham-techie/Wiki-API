00d2f3c285aa88f17bf15a04c454e31e71bc85c0
shubham-techie <85562020+shubham-techie@users.noreply.github.com>
2021-07-18 21:17:28 +0530

    Added get,put,patch and delete requests for specific article.

--- app.js
+++ app.js
@@ -19,42 +19,104 @@ const articleSchema = {
 
 const Article = new mongoose.model("Article", articleSchema);
 
+app.route("/articles")
+    .get(function (req, res) {
+        Article.find(function (err, foundArticle) {
+            if (!err) {
+                res.send(foundArticle);
+            } else {
+                res.send(err);
+            }
+        });
+    })
 
-app.get("/articles", function (req, res) {
-    Article.find(function (err, foundArticle) {
-        if (!err) {
-            res.send(foundArticle);
-        } else {
-            res.send(err);
-        }
+    .post(function (req, res) {
+
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+
+        newArticle.save(function (err) {
+            if (!err) {
+                res.send("Successfully added a new article.");
+            } else {
+                res.send(err);
+            }
+        });
+    })
+
+    .delete(function (req, res) {
+        Article.deleteMany(function (err) {
+            if (!err) {
+                res.send("Successfully deleted all articles.");
+            } else {
+                res.send(err);
+            }
+        });
     });
-});
 
-app.post("/articles", function (req, res) {
 
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
+
+app.route("/articles/:articleTitle")
+    .get((req, res) => {
+        Article.findOne({ title: req.params.articleTitle }, function (err, foundArticle) {
+            if (foundArticle) {
+                res.send(foundArticle);
+            } else {
+                res.send("No such Article is found with that title." + err);
+            }
+        });
+    })
+
+
+    .put((req, res) => {
+        Article.replaceOne(
+            { title: req.params.articleTitle },
+            {
+                title: req.body.title,
+                content: req.body.content
+            },
+            function (err, foundArticle) {
+                if (!err) {
+                    res.send("Successfully updated a article.");
+                } else {
+                    res.send("No such Article is found with that title." + err);
+                }
+            }
+        );
+    })
+
+
+    .patch((req, res) => {
+        Article.updateOne(
+            { title: req.params.articleTitle },
+            { $set: req.body },
+            function (err, foundArticle) {
+                if (!err) {
+                    res.send("Successfully updated article.");
+                } else {
+                    res.send("No such Article is found with that title." + err);
+                }
+            }
+        );
+    })
+
+
+    .delete((req, res) => {
+        Article.deleteOne(
+            { title: req.params.articleTitle },
+            function (err) {
+                if (!err) {
+                    res.send("Successfully deleted a article.");
+                } else {
+                    res.send("No such Article is found with that title." + err);
+                }
+            }
+        );
     });
 
-    newArticle.save(function (err) {
-        if (!err) {
-            res.send("Successfully added a new article.");
-        } else {
-            res.send(err);
-        }
-    });
-});
 
-app.delete("/articles", function (req, res) {
-    Article.deleteMany(function (err) {
-        if (!err) {
-            res.send("Successfully deleted all articles.");
-        } else {
-            res.send(err);
-        }
-    });
-});
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000");
