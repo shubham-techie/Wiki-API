7a0f51df7b008f015d91b18bf1bc5f821225fc53
shubham-techie <85562020+shubham-techie@users.noreply.github.com>
2021-07-18 21:56:35 +0530

    Removed unwanted files.

--- app.js
+++ app.js
@@ -119,5 +119,5 @@ app.route("/articles/:articleTitle")
 
 
 app.listen(3000, function () {
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port 3000.");
 });
