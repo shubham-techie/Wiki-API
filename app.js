ef1e85a19b5cdf333ccd278b1375c0c773a315f1
shubham-techie <85562020+shubham-techie@users.noreply.github.com>
2021-07-18 19:45:37 +0530

    Added post and delete requests for Wiki-API

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost:27017/wikiDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -25,7 +24,33 @@ app.get("/articles", function (req, res) {
     Article.find(function (err, foundArticle) {
         if (!err) {
             res.send(foundArticle);
-        }else{
+        } else {
+            res.send(err);
+        }
+    });
+});
+
+app.post("/articles", function (req, res) {
+
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    newArticle.save(function (err) {
+        if (!err) {
+            res.send("Successfully added a new article.");
+        } else {
+            res.send(err);
+        }
+    });
+});
+
+app.delete("/articles", function (req, res) {
+    Article.deleteMany(function (err) {
+        if (!err) {
+            res.send("Successfully deleted all articles.");
+        } else {
             res.send(err);
         }
     });
